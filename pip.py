e36fcd1f768d5d63f8a365385adbb9e9ce09476e
thiiim <134673353+thiiim@users.noreply.github.com>
2025-04-25 14:30:44 +0700

    Add files via upload

---
+++ pip.py
@@ -0,0 +1,6 @@
+from stringcolor import *
+print(cs("Selamat Datang", "blue", "white"))
+print(bold("Selamat Datang"))
+print(underline("Selamat Datang"))
+print(underline("Selamat Datang").bold())
+print(cs("Selamat Datang", "Blue", "Yellow").underline())
